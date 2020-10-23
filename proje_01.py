fc8cc3101a7dd2360a31f04053012076650f9739
Zeynep Arslan <61504059+zeynepnua@users.noreply.github.com>
2020-10-23 09:05:30 +0300

    Update proje_01.py

--- proje_01.py
+++ proje_01.py
@@ -1,6 +1,12 @@
-cümle = "elbise çok güzel"
+import string
+cümle = "Elbise çok güzel değil mi ? "
+yenicümle = ""
+for i in cümle :
+  if i not in string.punctuation:
+    yenicümle += i
+print(yenicümle)
 kelime = str(input("kelime giriniz: "))
-arama = cümle.find(kelime)
+arama = yenicümle.find(kelime)
 if arama > -1 :
  print(arama," = indeks numarası ve cümle içinde var")
 elif arama == -1 :
